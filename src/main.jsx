18c97598466b7bfaf4712896a04331653bea848a
Manoj Prabakaran <manoj7prabakaran@gmail.com>
2026-08-06 20:21:43 +0530

    Deploy React app to GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/myportfolio">
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
