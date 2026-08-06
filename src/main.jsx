1349f561f544e3abfd84239b6b9845e3ebd089ef
Manoj Prabakaran <manoj7prabakaran@gmail.com>
2026-08-06 19:51:51 +0530

    Deploy React app

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/myportfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
